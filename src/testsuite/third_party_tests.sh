61c25e12b84ad8d07bc9d16ed671da1d1408608e
Ken Burtch <ken@pegasoft.ca>
2018-03-18 21:00:25 -0400

    added xvfb SDL examples coverage test

--- src/testsuite/third_party_tests.sh
+++ src/testsuite/third_party_tests.sh
@@ -100,6 +100,26 @@ else
 fi
 }
 
+# A minimal test of SDL using example programs in an
+# X virtual frame buffer
+# Doesn't check the output because "Press Return" is sent
+# back.
+
+do_sdl_xvfb_test() {
+TESTSET="sdl_xvfb.sh"
+if [ ! -f "$TESTSET" ] ; then
+   echo "Failed - $TESTSET is missing"
+fi
+RESULT=`bash ./$TESTSET 2>&1`
+if [ $? -ne 0 ] ; then
+   echo "Failed - $TESTSET Failed"
+   echo "$RESULT"
+   exit 192
+else
+   echo "OK - $TESTSET"
+fi
+}
+
 echo "$0: Testing thrid-party software..."
 echo "$0: "`date`
 
@@ -110,6 +130,7 @@ do_cgi_good_test
 do_btree_good_test
 do_hash_good_test
 do_memcache_good_test
+do_sdl_xvfb_test
 
 # ---------------------------------------------------------------------------
 
