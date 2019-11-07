7e9890482e7609efc7d6c1ffb8fd509d98a516ea
Ken Burtch <ken@pegasoft.ca>
2019-11-06 23:34:00 -0500

    kludge to get constants working at compile time.  the need for static expressions is noted in the todo file already

--- src/parser-decl.adb
+++ src/parser-decl.adb
@@ -1519,6 +1519,7 @@ begin
        -- Calculate the assignment (ie. using any previous variable i)
 
        ParseAssignPart( expr_value, right_type );          -- do := part
+       put_trace( "expression value " & to_string( expr_value ) ); -- CONST SPECS DEBUG
 
        -- Redeclare temporarily destroyed identifier (ie. declare new i)
        -- and assign its type
@@ -1533,14 +1534,14 @@ begin
        end if;
      end;
 
-    -- exceptions are a special case because they are a keyword
+     -- exceptions are a special case because they are a keyword
 
-    if right_type = exception_t then
-       err( "exceptions cannot be assigned" );
+     if right_type = exception_t then
+        err( "exceptions cannot be assigned" );
 
-    -- command types have special limitations
+     -- command types have special limitations
 
-    elsif getBaseType( type_token ) = command_t then
+     elsif getBaseType( type_token ) = command_t then
        if baseTypesOK( uni_string_t, right_type ) then
           type_token := uni_string_t; -- pretend it's a string
           if not C_is_executable_file( to_string( expr_value ) & ASCII.NUL ) then
@@ -1555,6 +1556,7 @@ begin
 
      -- perform assignment
 
+     --if isExecutingCommand then
      if isExecutingCommand then
         expr_value := castToType( expr_value, type_token );
         if type_token /= right_type then
@@ -1565,6 +1567,19 @@ begin
             put_trace(
                to_string( identifiers( id ).name ) & " := """ &
                to_string( ToEscaped( expr_value ) ) & """" );
+        end if;
+     else
+        -- At compile time, constants may have assigned values.
+        -- However, we do not have static expression support (yet).
+        -- A variable assigned to a constant may not be defined
+        -- until run-time.
+        -- KLUDGE: For constants, we may not be running.  Contracts
+        -- cannot be run.
+        if identifiers( id ).usage = constantUsage then
+           if expr_value /= null_unbounded_string then
+              expr_value := castToType( expr_value, type_token );
+              identifiers( id ).value.all := expr_value;
+           end if;
         end if;
      end if;
   elsif (token = symbol_t and identifiers( token ).value.all = ";") then
