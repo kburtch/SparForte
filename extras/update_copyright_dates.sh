8e201e19fc49a7d2c0912fe577f6594a9569b80d
Ken Burch <ken@pegasoft.ca>
2026-02-14 16:12:22 -0500

    updated copyright dates to 2026

--- extras/update_copyright_dates.sh
+++ extras/update_copyright_dates.sh
@@ -1,6 +1,6 @@
-ls *.adb | (while read FILE ; do
+ls *.ads | (while read FILE ; do
 
-  sed 's/2001-2024/2001-2025/g' < "$FILE" > t.t
+  sed 's/2001-2025/2001-2026/g' < "$FILE" > t.t
   mv t.t "$FILE"
 done)
 
